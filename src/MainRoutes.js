e486d282fb715cf6773d00bfe14a19c99eb21a70
Arsen2003 <arsen_nurdavletov@mail.ru>
2022-08-23 17:21:58 +0600

    .

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Route,Routes } from 'react-router-dom'
-import Regestration from './pagess/Regestration'
-import Home from './pagess/Home'
+import { Route, Routes } from 'react-router-dom'
+import Regestration from './pages/Regestration'
+import Home from './pages/Home'
 const MainRoutes = () => {
   return (
     <div>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/regestration' element={<Regestration/>}/>
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/regestration" element={<Regestration />} />
+      </Routes>
     </div>
   )
 }
