7e14fe2070b7dd704ce517ac11a6fea1217413c9
catab1e <tilektursunbaiuulu@gmail.com>
2022-08-26 15:41:21 +0600

    add music list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
+import Footer from "./components/Footer";
+import MusicsList from "./components/MusicsList";
 import MainRoutes from "./MainRoutes";
 
 function App() {
   return (
     <>
       <MainRoutes />
-
+      <MusicsList/> 
+      <Footer/>
     </>
   );
 }
--- src/components/MusicsList.jsx
+++ src/components/MusicsList.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React from "react";
+import "../style/music.css";
+import spotify from "../assets/icons/spotify.svg"
 
 const MusicsList = () => {
-    return (
-        <div className='musics'>
-            <div className='music'>
-                <span> number </span>
-            </div>
+  return (
+    <>
+    <div className="razmetka">
+        <span id="number">#</span>
+        <span id="name">Название</span>
+        <span id="alb">Альбом</span>
+    </div>
+    <hr />
+    <div className="musics">
+      <div className="music">
+        <span id="number"> 1 </span>
+        <img className="mus__photo" src={spotify} alt=""/>
+        <div className="names__of__song">
+            <p>NAME</p>
+            <p className="sub__name">SUBNAME</p>
         </div>
-    );
+        <span id="alb2"> album </span>
+      </div>
+
+      <div className="music">
+        <span id="number"> 1 </span>
+        <img className="mus__photo" src={spotify} alt=""/>
+        <div className="names__of__song">
+            <p>NAME</p>
+            <p className="sub__name">SUBNAME</p>
+        </div>
+        <span id="alb2"> album </span>
+      </div>
+
+      <div className="music">
+        <span id="number"> 1 </span>
+        <img className="mus__photo" src={spotify} alt=""/>
+        <div className="names__of__song">
+            <p>NAME</p>
+            <p className="sub__name">SUBNAME</p>
+        </div>
+        <span id="alb2"> album </span>
+      </div>
+
+      <div className="music">
+        <span id="number"> 1 </span>
+        <img className="mus__photo" src={spotify} alt=""/>
+        <div className="names__of__song">
+            <p>NAME</p>
+            <p className="sub__name">SUBNAME</p>
+        </div>
+        <span id="alb2"> album </span>
+      </div>
+
+      <div className="music">
+        <span id="number"> 1 </span>
+        <img className="mus__photo" src={spotify} alt=""/>
+        <div className="names__of__song">
+            <p>NAME</p>
+            <p className="sub__name">SUBNAME</p>
+        </div>
+        <span id="alb2"> album </span>
+      </div>
+    </div>
+    </>
+  );
 };
 
 export default MusicsList;
